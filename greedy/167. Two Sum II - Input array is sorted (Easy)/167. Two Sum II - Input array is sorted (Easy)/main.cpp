fc3b7109ca76fe8456fc49b69eef6315f6b2a06a
Streambuns <flyingpig@nwsuaf.edu.cn>
2022-09-18 19:52:48 +0800

    done 167

--- greedy/167. Two Sum II - Input array is sorted (Easy)/167. Two Sum II - Input array is sorted (Easy)/main.cpp
+++ greedy/167. Two Sum II - Input array is sorted (Easy)/167. Two Sum II - Input array is sorted (Easy)/main.cpp
@@ -27,5 +27,21 @@ int main(int argc, const char * argv[]) {
 }
 vector<int> twoSum(vector<int>& numbers, int target){
     vector<int> returnArray;
+    sort(numbers.begin(),numbers.end());
+    for(int i=0;i<numbers.size();){
+        for(int j=(int)numbers.size()-1;j>i;){
+            if(numbers[i]+numbers[j]==target){
+                returnArray.push_back(i+1);
+                returnArray.push_back(j+1);
+                return returnArray;
+            }
+            else if(numbers[i]+numbers[j]>target){
+                j--;
+            }
+            else if(numbers[i]+numbers[j]<target){
+                i++;
+            }
+        }
+    }
     return returnArray;
 }
