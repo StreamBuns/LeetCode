f7f1879a2673bf75404584b02c8703ad5c860484
Streambuns <flyingpig@nwsuaf.edu.cn>
2022-09-19 22:13:39 +0800

    滑动窗口

--- 3doublePointer/76. Minimum Window Substring (Hard)/76. Minimum Window Substring (Hard)/main.cpp
+++ 3doublePointer/76. Minimum Window Substring (Hard)/76. Minimum Window Substring (Hard)/main.cpp
@@ -6,9 +6,29 @@
 //
 
 #include <iostream>
+#include <vector>
 
+using namespace std;
+string minWindow(string s, string t) ;
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    string s,t;
+    cin>>s>>t;
+//    cout<<minWindow(s,t)<<endl;
+    cout<<s.find("a")<<endl;
     return 0;
+}
+string minWindow(string s, string t) {
+    string minStr;
+    unsigned long path;
+    for(int i=0;i<s.size();i++){
+        int flag[t.size()];
+        path=t.find(s[i]);
+        if(path!=string::npos){
+            flag[path]=1;
+            
+        }
+    }
+    
+    return minStr;
 }
