972c6862649f95c49c02172d1a38b37483f199cb
Streambuns <flyingpig@nwsuaf.edu.cn>
2022-09-20 23:14:08 +0800

    create 69

---
+++ 4binarySearch/69. Sqrt(x) (Easy)/69. Sqrt(x) (Easy)/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  69. Sqrt(x) (Easy)
+//
+//  Created by 赵桐 on 2022/9/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
