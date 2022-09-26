c8694f9e7bb7837e776f279d1da4bb4e50de9bab
Streambuns <flyingpig@nwsuaf.edu.cn>
2022-09-26 23:23:20 +0800

    done 69

--- 4binarySearch/69. Sqrt(x) (Easy)/69. Sqrt(x) (Easy)/main.cpp
+++ 4binarySearch/69. Sqrt(x) (Easy)/69. Sqrt(x) (Easy)/main.cpp
@@ -6,9 +6,27 @@
 //
 
 #include <iostream>
-
+int mySqrt(int x);
+using namespace std;
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    int number=2147395599;
+    std::cout << mySqrt(number);
     return 0;
+}
+int mySqrt(int x) {
+    if(x<=0)return 0;
+    long long int l=1,r=x;
+    long long int mid=(l+r)/2;
+    while(!(mid*mid<=x&&(mid+1)*(mid+1)>x)){
+        if(mid*mid>x){
+            r=mid;
+            mid=(l+mid)/2;
+        }
+        else{
+            l=mid;
+            mid=(r+mid)/2;
+        }
+    }
+    return mid;
 }
